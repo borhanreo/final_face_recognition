edb2d4783e8d3af0479c60fad134adb1eb452d9b
borhan <borhan.u.cse@gmail.com>
2019-05-27 14:21:40 +0600

    Autonomous function
    Add an Autonomous function for read image and compare with other image

--- face_recog.py
+++ face_recog.py
@@ -13,44 +13,59 @@ video_capture = cv2.VideoCapture(0)
 # Create a woorksheet
 book=Workbook()
 sheet=book.active
-    
-# Load images.
-    
+thisdir = os.getcwd()
+# known_face_encodings = []
+# known_face_names_txt = []
+# known_face_names = []
+#
+# for r,d,f in os.walk(thisdir):
+#     counter = 1
+#     for file in f:
+#         if ".jpeg" in file:
+#             image_borhan = face_recognition.load_image_file(os.getcwd() + "/image/"+file)
+#             image_borhan_face_encoding = face_recognition.face_encodings(image_borhan)[0]
+#             known_face_encodings.append(image_borhan_face_encoding)
+#             known_face_names_txt.append( os.path.splitext(file)[0])
+#             known_face_names.append(str(counter))
+#             print(counter,os.path.splitext(file)[0])
+#             counter+=1
+# # Load images.
+#
 image_borhan = face_recognition.load_image_file(os.getcwd()+"/image/borhan.jpeg")
 image_borhan_face_encoding = face_recognition.face_encodings(image_borhan)[0]
-    
+
 image_dada = face_recognition.load_image_file(os.getcwd()+"/image/dada.jpeg")
 image_dada_face_encoding = face_recognition.face_encodings(image_dada)[0]
-    
+
 image_jason = face_recognition.load_image_file(os.getcwd()+"/image/jason.jpeg")
 image_jason_face_encoding = face_recognition.face_encodings(image_jason)[0]
-    
+
 image_soumith = face_recognition.load_image_file(os.getcwd()+"/image/soumith.jpeg")
 image_soumith_face_encoding = face_recognition.face_encodings(image_soumith)[0]
 
 image_forhad = face_recognition.load_image_file(os.getcwd()+"/image/forhad.jpeg")
 image_forhad_face_encoding = face_recognition.face_encodings(image_forhad)[0]
-    
-    
+
+
 # Create arrays of known face encodings and their names
 known_face_encodings = [
-        
+
         image_borhan_face_encoding,
         image_dada_face_encoding,
         image_jason_face_encoding,
         image_soumith_face_encoding,
         image_forhad_face_encoding
-        
+
     ]
 known_face_names = [
-        
+
         "1",
         "2",
         "3",
         "4",
         "5"
     ]
-    
+
 # Initialize some variables
 face_locations = []
 face_encodings = []
@@ -83,7 +98,6 @@ while True:
     for face_encoding in face_encodings:
         # See if the face is a match for the known face(s)
         matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-        global name
         name = "Unknown"
     
         # If a match was found in known_face_encodings, just use the first one.
@@ -92,12 +106,13 @@ while True:
             name = known_face_names[first_match_index]
             # Assign attendance
             if int(name) in range(1, 61):
-                sheet.cell(row=int(name), column=int(today)).value = "Present"
+                #sheet.cell(row=int(name), column=int(today)).value = "Present"
+                sheet.cell(row=int(name), column=int(1)).value = "Present"
             else:
                 pass
     
         face_names.append(name)
-        print("   ", name)
+        #print("   ", name)
     process_this_frame = not process_this_frame
     
     top=0
@@ -105,12 +120,15 @@ while True:
     bottom=0
     left=0
     # Display the results
+    original_name = "A"
     for (top, right, bottom, left), name in zip(face_locations, face_names):
            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
            top *= 4
            right *= 4
            bottom *= 4
            left *= 4
+           #print(name)
+           original_name = name #known_face_names_txt[int(name)]
     
     # Draw a box around the face
     cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
@@ -118,7 +136,7 @@ while True:
            # Draw a label with a name below the face
     cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
     font = cv2.FONT_HERSHEY_DUPLEX
-    cv2.putText(frame, "A", (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+    cv2.putText(frame, original_name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
     
     # Display the resulting image
     cv2.imshow('Video', frame)
--- test.py
+++ test.py
@@ -1,2 +1,22 @@
+import face_recognition
+import cv2
+from openpyxl import Workbook
+import datetime
 import os
 print(os.getcwd())
+thisdir = os.getcwd()
+known_face_encodings = []
+known_face_names_txt = []
+known_face_names = []
+
+for r,d,f in os.walk(thisdir):
+    counter = 1
+    for file in f:
+        if ".jpeg" in file:
+            image_borhan = face_recognition.load_image_file(os.getcwd() + "/image/borhan.jpeg")
+            image_borhan_face_encoding = face_recognition.face_encodings(image_borhan)[0]
+            known_face_encodings.append(image_borhan_face_encoding)
+            known_face_names_txt.append( os.path.splitext(file)[0])
+            known_face_names.append(counter)
+            print(counter,os.path.splitext(file)[0])
+            counter+=1
